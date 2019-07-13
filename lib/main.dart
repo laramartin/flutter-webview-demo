d0e6e3a84cc1818521ce427380c66d86074244eb
Lara Martin <lara.martin.carretero@gmail.com>
2019-07-13 20:32:55 +0200

    Define list items

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:resources_to_learn_flutter/data/resource_item.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,9 +23,12 @@ class MyApp extends StatelessWidget {
           title: Text("Resources to learn"),
           leading: FlutterLogo(),
         ),
-        body: ListView(
-          children:
-              resourceItems.map((item) => ResourceListItem(item)).toList(),
+        body: Container(
+          color: Color(0xffffebee),
+          child: ListView(
+            children:
+                resourceItems.map((item) => ResourceListItem(item)).toList(),
+          ),
         ),
       ),
     );
@@ -38,9 +42,53 @@ class ResourceListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ExpansionTile(
-      title: Text(resourceItem.title),
+    print("Item ${resourceItem.title}, url: ${resourceItem.url}");
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 0),
+      child: Column(
+        children: <Widget>[
+          Card(
+            child: ResourceCardContent(resourceItem),
+            elevation: 8,
+            color: Color(0xffe0f7fa),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
+class ResourceCardContent extends StatelessWidget {
+  ResourceCardContent(this.item);
+
+  final ResourceItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            item.title,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+        Container(
+          height: 200.0,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: WebView(
+              initialUrl: item.url,
+              javascriptMode: JavascriptMode.unrestricted,
+              onPageFinished: (url) => print("finished loading: $url"),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
