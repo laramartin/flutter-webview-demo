b06c49969026b3d1caeab1b8d1ec9833d1961f9b
Lara Martin <lara.martin.carretero@gmail.com>
2019-07-13 19:06:09 +0200

    Add list of ExpansionTile items with headers

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:resources_to_learn_flutter/data/resource_item.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final resourceItems = resourcesList;
+
     return MaterialApp(
       title: 'Resources to learn Flutter',
-      theme: ThemeData(primarySwatch: Colors.blue,
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Resources to learn")
+          title: Text("Resources to learn"),
+          leading: FlutterLogo(),
+        ),
+        body: ListView(
+          children:
+              resourceItems.map((item) => ResourceListItem(item)).toList(),
         ),
       ),
+    );
+  }
+}
+
+class ResourceListItem extends StatelessWidget {
+  final ResourceItem resourceItem;
+
+  ResourceListItem(this.resourceItem);
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      title: Text(resourceItem.title),
+
     );
   }
 }
